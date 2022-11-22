fbb5455706001ba1f2c163080bec3cda9b47b9c1
Togzhan <togjan997@mail.ru>
2022-11-23 01:21:17 +0200

    saved and close it

--- scriptLayout.js
+++ scriptLayout.js
@@ -17,7 +17,7 @@ const BoardSource = {
 const ovenConstant = {
   horizontalSlots: 3,
   subSlotLetters: ["A", "B", "C", "D", "E"],
-  verticalSlots: 10,
+  totalNumberSlot: 13,
 };
 
 const createSlot = () => {
@@ -29,14 +29,20 @@ const createSlot = () => {
 const createSubslot = (
   slotNumber,
   subSlotLetter,
+  active = false,
   state = "IDLE",
   time = ""
 ) => {
   const subSlot = document.createElement("div");
   subSlot.classList.add("subSlot", state);
 
+  active && subSlot.classList.add("selected");
   subSlot.addEventListener("click", (e) => {
-    subSlot.classList.toggle("selected");
+    subSlot.parentElement.querySelectorAll(".selected").forEach((selected) => {
+      selected.classList.remove("selected");
+    });
+
+    subSlot.classList.add("selected");
   });
 
   const timeRemaining = document.createElement("div");
@@ -63,27 +69,83 @@ const printOven = (ovenState) => {
     slot.classList.add("hSlot");
     slot.id = `slot${idSlot}`;
 
-    ovenConstant.subSlotLetters.forEach((letter) => {
-      const subSlot = createSubslot(idSlot, letter);
+    // creating subslots
+    ovenConstant.subSlotLetters.forEach((letter, index) => {
+      const data = ovenState.find(
+        (item) => item.slot === idSlot && item.subSlot === letter
+      );
+
+      const subSlot = data
+        ? createSubslot(idSlot, letter, index === 0, data?.state, data?.time)
+        : createSubslot(idSlot, letter, index === 0);
 
       slot.appendChild(subSlot);
     });
 
     ovenSctructure.appendChild(slot);
   }
+
+  // creating horizontal slots
+
+  const verticalSection = document.createElement("div");
+  verticalSection.classList.add("verticalSection");
+
+  for (
+    let idSlot = ovenConstant.horizontalSlots + 1;
+    idSlot <= ovenConstant.totalNumberSlot;
+    idSlot++
+  ) {
+    const data = ovenState.find((item) => item.slot === idSlot);
+
+    const time = data?.time || "";
+    const state = data?.state || "IDLE";
+
+    const slot = document.createElement("div");
+    slot.classList.add("vSlot", state);
+    slot.id = `slot${idSlot}`;
+
+
+    const timeRemaining = document.createElement("div");
+    timeRemaining.classList.add("timeRemaining");
+    timeRemaining.innerText = time;
+
+    const slotState = document.createElement("div");
+    slotState.classList.add("slotState");
+    slotState.innerText = state;
+
+    // nameSlot
+
+    const nameSlot = document.createElement("div");
+    nameSlot.classList.add("nameSlot");
+    nameSlot.innerText = `SLOT ${idSlot}`;
+
+    slot.appendChild(nameSlot);
+    slot.appendChild(timeRemaining);
+    slot.appendChild(slotState);
+
+    verticalSection.appendChild(slot);
+  }
+
+  ovenSctructure.appendChild(verticalSection);
 };
 
 const testOvenState = [
   {
-    id: "1-1",
     slot: 1,
-    subSlot: 1,
+    subSlot: "C",
     state: SlotState.BAKING,
-    timeStarted: new Date(),
-    timeEnd: new Date(),
-    operatorName: "John",
-    boardSerialNumber: "1234567890",
-    boardSource: BoardSource.SMA,
+    time: "00:59:01",
+  },
+  {
+    slot: 10,
+    state: SlotState.BAKING,
+    time: "00:59:01",
+  },
+  {
+    slot: 3,
+    subSlot: "A",
+    state: SlotState.BAKING,
+    time: "00:09:01",
   },
 ];
 
